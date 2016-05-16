15b89906b1485ff1b8d07b2abc35f7fcd6519750
Stefan Foulis <stefan@foulis.ch>
2016-05-16 04:44:01 +0200

    remove comments of aldryn-sso. ignore override

--- settings.py
+++ settings.py
@@ -5,7 +5,6 @@ INSTALLED_ADDONS = [
     # <INSTALLED_ADDONS>  # Warning: text inside the INSTALLED_ADDONS tags is auto-generated. Manual changes will be overwritten.
     'aldryn-addons',
     'aldryn-django',
-    # 'aldryn-sso',
     'aldryn-django-cms',
     'aldryn-celery',
     # </INSTALLED_ADDONS>
