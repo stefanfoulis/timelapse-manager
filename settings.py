dec7b21536c46c926ea07479fb4b6dfaa38910b3
Stefan Foulis <stefan.foulis@divio.ch>
2016-05-16 02:48:48 +0000

    install aldryn-addons 1.0.1

--- settings.py
+++ settings.py
@@ -3,6 +3,7 @@ from getenv import env
 
 INSTALLED_ADDONS = [
     # <INSTALLED_ADDONS>  # Warning: text inside the INSTALLED_ADDONS tags is auto-generated. Manual changes will be overwritten.
+    'aldryn-addons',
     'aldryn-django',
     'aldryn-django-cms',
     # </INSTALLED_ADDONS>
