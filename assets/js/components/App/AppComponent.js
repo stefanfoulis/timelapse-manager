b735330492d4edfda2a07d6f8248621857b78e82
Stefan Foulis <stefan@foulis.ch>
2016-06-17 23:22:16 +0200

    I can haz countdown

--- assets/js/components/App/AppComponent.js
+++ assets/js/components/App/AppComponent.js
@@ -4,6 +4,12 @@ import 'react-mdl/extra/css/material.cyan-red.min.css';
 import Navbar from '../Navbar/NavbarComponent';
 import Footer from '../Footer/FooterContainer';
 import styles from './App.scss';
+var Countdown = require('react-cntdwn');
+
+var handleFinish = function () {
+  console.log('Skynet has become self-aware!');
+}
+
 
 export default class App extends React.Component {
   static propTypes = {
@@ -17,7 +23,13 @@ export default class App extends React.Component {
         <Navbar />
         <div className={styles.greeting}>
           <h1 className={styles.sawasdee}>Sawasdee, Sawasdee!</h1>
-          <p>Always a pleasure scaffolding your apps</p>
+          <Countdown targetDate={new Date('September 30, 2016')}
+           startDelay={1000}
+           interval={1000}
+           timeSeparator={':'}
+           leadingZero
+           format={{day: 'DD', hour: 'hh', minute: 'mm', second: 'ss'}}
+           onFinished={handleFinish} />
         </div>
         <div className={styles.content}>
           {this.props.children}
